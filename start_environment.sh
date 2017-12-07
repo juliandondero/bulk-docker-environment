a8dd1ba307fba9d87c6e2df214e63acc05efc788
Julián Dondero <juliandondero@gmail.com>
2017-12-07 14:28:25 -0300

    force sails port to 1337

--- start_environment.sh
+++ start_environment.sh
@@ -3,5 +3,5 @@
 echo "Iniciando docker...";
 docker-compose start;
 
-echo "Iniciando sails..."
+echo "**** Iniciando sails en http://127.0.0.1:1337 ****"
 docker exec -u user -it bulkdockerenvironment_sails_1 bash -c "cd /home/user/workspace/go5-bulk/; source ~/.nvm/nvm.sh; yarn install; yarn start"
