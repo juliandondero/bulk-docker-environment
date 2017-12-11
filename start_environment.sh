1f93f92deb414a6e4d64a7ffd0ccd391a6e25ec5
Julián Dondero <juliandondero@gmail.com>
2017-12-11 12:33:14 -0300

    todos los workers a supervisord

--- start_environment.sh
+++ start_environment.sh
@@ -4,8 +4,8 @@ echo "Iniciando docker...";
 docker-compose start;
 
 echo "**** Iniciando workers ****"
-docker exec -u user -it bulkdockerenvironment_sails_1 bash -c "cd /home/user/workspace/go5-bulk/server/; source ~/.nvm/nvm.sh; yarn install; node index.js &"
-docker exec -u user -it bulkdockerenvironment_sails_1 bash -c "cd /home/user/workspace/go5-bulk/server/; source ~/.nvm/nvm.sh; yarn install; node loader.js &"
+docker exec -u user -it bulkdockerenvironment_sails_1 bash -c "cd /home/user/workspace/go5-bulk/server/; source ~/.nvm/nvm.sh; yarn install; node index.js > /dev/stdout &";
+docker exec -u user -it bulkdockerenvironment_sails_1 bash -c "cd /home/user/workspace/go5-bulk/server/; source ~/.nvm/nvm.sh; yarn install; node loader.js > /dev/stdout &";
 
 echo "**** Iniciando sails en http://127.0.0.1:1337 ****"
-docker exec -u user -it bulkdockerenvironment_sails_1 bash -c "cd /home/user/workspace/go5-bulk/; source ~/.nvm/nvm.sh; yarn install; yarn start"
+docker exec -u user -it bulkdockerenvironment_sails_1 bash -c "cd /home/user/workspace/go5-bulk/; source ~/.nvm/nvm.sh; yarn install; yarn start > /dev/stdout";
