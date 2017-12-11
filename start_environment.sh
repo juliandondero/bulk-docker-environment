7ebdd7266ed1a6f6dc3882952bac12193108ab5e
Julián Dondero <juliandondero@gmail.com>
2017-12-11 11:39:24 -0300

    levanto workers

--- start_environment.sh
+++ start_environment.sh
@@ -3,5 +3,9 @@
 echo "Iniciando docker...";
 docker-compose start;
 
+echo "**** Iniciando workers ****"
+docker exec -u user -it bulkdockerenvironment_sails_1 bash -c "cd /home/user/workspace/go5-bulk/server/; source ~/.nvm/nvm.sh; yarn install; node index.js &"
+docker exec -u user -it bulkdockerenvironment_sails_1 bash -c "cd /home/user/workspace/go5-bulk/server/; source ~/.nvm/nvm.sh; yarn install; node loader.js &"
+
 echo "**** Iniciando sails en http://127.0.0.1:1337 ****"
 docker exec -u user -it bulkdockerenvironment_sails_1 bash -c "cd /home/user/workspace/go5-bulk/; source ~/.nvm/nvm.sh; yarn install; yarn start"
